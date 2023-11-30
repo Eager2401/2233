8732da106956fdaaad004426b9266eb1c9f027e4
Hiếu Phạm <bosseager0175@gmail.com>
2023-11-30 10:57:55 +0700

    Add project files.

---
+++ 2233/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using _2233.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2233.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+        public ActionResult QuanLi()
+        {
+            var DanhSach = new San().KhoiTaoSan();
+            return View(DanhSach);
+        }
+        public ActionResult NguoiDung()
+        {
+            var DanhSach = new San().KhoiTaoSan();
+            return View(DanhSach);
+        }
+        public ActionResult ThemSan()
+        {
+            return View();
+        }
+        public ActionResult ThueSan()
+        {
+            return View();
+        }
+        public ActionResult SuaSan()
+        {
+            return View();
+        }
+        public ActionResult DongTatCaSan()
+        {
+            return View();
+        }
+        //public ActionResult LuuSan(int soSan,int giaThue1Gio,string tinhTrang) 
+        //{
+        //    DanhSachSan.DachSachSan.Add(new San()
+        //    {
+        //        SoSan = soSan,
+        //        GiaThue1Gio = giaThue1Gio,
+        //        TinhTrang = tinhTrang
+        //    }
+        //     ); 
+        //    return RedirectToAction("QuanLi");
+        //}
+    }
+}
---
+++ 2233/Models/San.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2233.Models
+{
+    public class San
+    {
+        public int SoSan { get; set; }
+        public int GiaThue1Gio { get; set; }
+        public string TinhTrang { get; set; }
+        public List<San> KhoiTaoSan()
+        {
+            List<San> dsSan = new List<San>();
+            dsSan.Add(new San()
+            {
+                SoSan = 1,
+                GiaThue1Gio = 50000,
+                TinhTrang = "Trong",
+            });
+            dsSan.Add(new San()
+            {
+                SoSan = 2,
+                GiaThue1Gio = 51000,
+                TinhTrang = "Trong",
+            });
+            dsSan.Add(new San()
+            {
+                SoSan = 3,
+                GiaThue1Gio = 52000,
+                TinhTrang = "Trong",
+            });
+            dsSan.Add(new San()
+            {
+                SoSan = 4,
+                GiaThue1Gio = 53000,
+                TinhTrang = "Trong",
+            });
+            return dsSan;
+        }
+    }
+}
